b76f63814b031c5b0217c72e6108a0d9c25caff2
Pavan <pavanreddy011@gmail.com>
2017-10-22 10:37:47 -0700

    Sprinkling water distribution

---
+++ CiruclarQueue.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparklingWater
+{
+    enum item
+    {
+        Berry1,
+        Berry2,
+        Berry3,
+        Berry4,
+        Water1,
+        Water2,
+        Water3,
+        Water4,
+        Citrus1,
+        Citrus2,
+        Citrus3,
+        Citrus4,
+        Cocunut
+    }
+
+    
+    class CiruclarQueue
+    {
+        public int length;
+        public item[] it=new item[13];
+
+        public CiruclarQueue(int len,item[] tmp)
+        {
+            length = len;
+            
+            for (int i = 0; i < length; i++)
+            {
+                it[i] = tmp[i];
+            }
+        }
+
+        public int findCocunut()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (it[i] == item.Cocunut)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void FinalDistribution()
+        {
+            int index=findCocunut()+1;
+
+
+            Console.WriteLine("Coconut is in "+index+" place so will start ");
+            Console.WriteLine(" Final Fair distribution is ...\n");
+
+            for (int i = 0; i < length; i++)
+            {
+                if (index >= length)
+                {
+                    index = 0;
+                }
+
+                Console.WriteLine((i+1) + "  " + it[index]);
+                index++;
+            }
+        }
+    }
+}
--- Source.cs
+++ Source.cs
@@ -4,28 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Maps
+namespace SparklingWater
 {
     class Source
     {
         static void Main(string[] args)
         {
-            //2017-01-01; Coffee Shop; L2, L5, L5, R5, L2      ------      (R5,R10)
-            //2017-01-02; Advertising Agency; R3, R3, R3, L2   ------      (R0,R5)
-
-            //(+ve, -ve) == (3,-2)
-            //2017-01-01; Coffee Shop; L1, L1, R1, L1, R1, L1, L1 ----      (L2,R3)
-
-            //(-ve, +ve) == (-3,2)
-            //2017-01-01; Coffee Shop; L1, R1, R1, L1, R1, L1, R1 -------   (R2,L3)
-
-            //(-ve, -ve) == (-3,-2)
-            //2017-01-01; Coffee Shop; L1, R1, L1, R2             ----      (L2,R3,)  
-
-            String str = "2017-01-01; Coffee Shop; L2, L5, L5, R5, L2 ";
+            item[] it = new item[13] { item.Berry1, item.Berry2, item.Berry3, item.Berry4,
+                                       item.Citrus1, item.Citrus2, item.Citrus3,item.Cocunut,
+                                       item.Citrus4, item.Water1, item.Water2, item.Water3, item.Water4};
             
-            Solution2 sol = new Solution2(str);
-            sol.solve();
+
+            CiruclarQueue cq = new CiruclarQueue(13,it);
+            cq.FinalDistribution();
         }
     }
 }
